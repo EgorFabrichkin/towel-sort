241d109385e14c4e395b7831c75833f00a7e2b7e
EgorFabrichkin <70843062+EgorFabrichkin@users.noreply.github.com>
2020-10-15 21:36:09 +0300

    correct mistake

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  let arr = [];
-
-  for (let i = 0; i < matrix.length; i++) {
-    if (i % 2 === 0) {
-      arr.push(matrix[i]);
+    let arr = [];
+    if (matrix === undefined || matrix.length === 0) {
+        return [];
     } else {
-      arr.push(matrix[i].sort((a, b) => b - a)); 
+        for (let i = 0; i < matrix.length; i++) {
+            if (i % 2 === 0) {
+                arr.push(matrix[i]);
+            } else {
+                arr.push(matrix[i].sort((a, b) => b - a)); 
+            }
+        }
+        return arr.flat();
     }
-  }
-  return arr.flat();
 }
